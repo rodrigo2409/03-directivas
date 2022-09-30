038a5e7bca54d6a073f1b3ab7fb3ddd55d7db0d7
rodrigo2409 <108556826+rodrigo2409@users.noreply.github.com>
2022-09-29 20:30:39 -0500

    primera version 03-directivast

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>directivas en vue.js</h1>
+    <hr />
+    <h2>v.tex</h2>
+   
+
+    <span v-text="`hola desde la directiva v-text`">      </span>
+    <br>
+    <span v-text="titulo"> </span>
+      <br>
+      esta es una trasmutacion de texto con una variable prop
+      <br>
+      {{titulo}}
+
+    
+  </div>
+
+</template>
+<script>
+    export default {
+        data: () => ({
+            titulo: "hola desde VUE.js",
+        }),
+    };
+</script>
+<style>
+    div {
+        text-align: center;
+        margin: 0px 2rem;
+    }
+</style>
